49f6966e4f7394c3d413ae7a2a44bc53f683b566
NCEX <73142334+NCEX@users.noreply.github.com>
2020-11-27 18:45:10 +0100

    Update client.rb

--- lib/peatio/cbn/client.rb
+++ lib/peatio/cbn/client.rb
@@ -1,14 +1,12 @@
-# frozen_string_literal: true
-
-require "memoist"
-require "faraday"
-require "better-faraday"
+require 'memoist'
+require 'faraday'
+require 'better-faraday'
 
 module Peatio
   module Cbn
     class Client
       Error = Class.new(StandardError)
-      ConnectionError = Class.new(Error)
+      class ConnectionError < Error; end
 
       class ResponseError < Error
         def initialize(code, msg)
@@ -27,38 +25,64 @@ module Peatio
         @json_rpc_endpoint = URI.parse(endpoint)
       end
 
-      def json_rpc(method, params=[])
-        response = post(method, params)
-
+      def json_rpc(method, params = [])
+        response = connection.post \
+          '/',
+          { jsonrpc: '1.0', method: method, params: params }.to_json,
+          { 'Accept'       => 'application/json',
+            'Content-Type' => 'application/json' }
         response.assert_2xx!
         response = JSON.parse(response.body)
-
-        response["error"].tap do |e|
-          raise ResponseError.new(e["code"], e["message"]) if e
+        response['error'].tap { |e| raise ResponseError.new(e['code'], e['message']) if e }
+        response.fetch('result')
+      rescue => e
+        if e.is_a?(Error)
+          raise e
+        elsif e.is_a?(Faraday::Error)
+          raise ConnectionError, e
+        else
+          raise Error, e
         end
+      end
 
-        response.fetch("result")
-      rescue Faraday::Error => e
-        raise ConnectionError, e
+      def json_rpc_for_withdrawal(method, address, amount)
+        response = connection.post \
+        '/',
+        { jsonrpc: '1.0', method: method, params: [
+            address,
+            amount.to_f,
+        # '', REMOVED! because protocol dosent support this para
+        # '', REMOVED! because protocol dosent support this para
+        # options[:subtract_fee].to_s == 'true'  # subtract fee from transaction amount.
+        ]}.to_json,
+        { 'Accept'       => 'application/json',
+          'Content-Type' => 'application/json' }
+        response.assert_2xx!
+        response = JSON.parse(response.body)
+        response['error'].tap { |e| raise ResponseError.new(e['code'], e['message']) if e }
+        response.fetch('result')
+      rescue => e
+        if e.is_a?(Error)
+          raise e
+        elsif e.is_a?(Faraday::Error)
+          raise ConnectionError, e
+        else
+          raise Error, e
+        end
       end
 
       private
 
-      def post(method, params)
-        connection.post("/", {jsonrpc: "1.0", method: method, params: params}.to_json,
-                        "Accept" => "application/json", "Content-Type" => "application/json")
-      end
-
       def connection
-        @connection ||= Faraday.new(@json_rpc_endpoint) do |f|
-          f.adapter :net_http_persistent, pool_size: 5
-        end.tap do |connection|
+        Faraday.new(@json_rpc_endpoint).tap do |connection|
           unless @json_rpc_endpoint.user.blank?
             connection.basic_auth(@json_rpc_endpoint.user,
                                   @json_rpc_endpoint.password)
           end
         end
       end
+      memoize :connection
     end
   end
 end
+
