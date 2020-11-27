c535b3419ff08ea656522d8af7269ed5cf07b7c8
NCEX <73142334+NCEX@users.noreply.github.com>
2020-11-27 18:41:47 +0100

    Update blockchain.rb

--- lib/peatio/cbn/blockchain.rb
+++ lib/peatio/cbn/blockchain.rb
@@ -1,17 +1,16 @@
-# frozen_string_literal: true
-
 module Peatio
   module Cbn
     # TODO: Processing of unconfirmed transactions from mempool isn't supported now.
     class Blockchain < Peatio::Blockchain::Abstract
+
       DEFAULT_FEATURES = {case_sensitive: true, cash_addr_format: false}.freeze
 
-      def initialize(custom_features={})
+      def initialize(custom_features = {})
         @features = DEFAULT_FEATURES.merge(custom_features).slice(*SUPPORTED_FEATURES)
         @settings = {}
       end
 
-      def configure(settings={})
+      def configure(settings = {})
         # Clean client state during configure.
         @client = nil
         @settings.merge!(settings.slice(*SUPPORTED_SETTINGS))
@@ -20,15 +19,13 @@ module Peatio
       def fetch_block!(block_number)
         block_hash = client.json_rpc(:getblockhash, [block_number])
 
-        block_txs = client.json_rpc(:getblock, [block_hash, 2])
-                          .fetch("tx").each_with_object([]) do |tx, txs_array|
+        client.json_rpc(:getblock, [block_hash, true])
+          .fetch('tx').each_with_object([]) do |tx, txs_array|
           txs = build_transaction(tx).map do |ntx|
             Peatio::Transaction.new(ntx.merge(block_number: block_number))
           end
           txs_array.append(*txs)
-        end
-
-        Peatio::Block.new(block_number, block_txs)
+        end.yield_self { |txs_array| Peatio::Block.new(block_number, txs_array) }
       rescue Client::Error => e
         raise Peatio::Blockchain::ClientError, e
       end
@@ -39,12 +36,14 @@ module Peatio
         raise Peatio::Blockchain::ClientError, e
       end
 
-      def load_balance_of_address!(address, _currency_id)
+      def load_balance_of_address!(address)
         address_with_balance = client.json_rpc(:listaddressgroupings)
-                                     .flatten(1)
-                                     .find {|addr| addr[0] == address }
+                                 .flatten(1)
+                                 .find { |addr| addr[0] == address }
 
-        raise Peatio::Blockchain::UnavailableAddressBalanceError, address if address_with_balance.blank?
+        if address_with_balance.blank?
+          raise Peatio::Blockchain::UnavailableAddressBalanceError, address
+        end
 
         address_with_balance[1].to_d
       rescue Client::Error => e
@@ -53,18 +52,19 @@ module Peatio
 
       private
 
-      def filter_vout(tx_hash)
-        tx_hash.fetch("vout").select do |entry|
-          entry.fetch("value").to_d.positive? && entry["scriptPubKey"].has_key?("addresses")
-        end
-      end
-
       def build_transaction(tx_hash)
-        filter_vout(tx_hash).each_with_object([]) do |entry, formatted_txs|
-          no_currency_tx = {hash: tx_hash["txid"], txout: entry["n"],
-                            to_address: entry["scriptPubKey"]["addresses"][0],
-                            amount: entry.fetch("value").to_d,
-                            status: "success"}
+        tx_hash = client.json_rpc(:getrawtransaction, [tx_hash, 1])
+        tx_hash.fetch('vout')
+          .select do |entry|
+          entry.fetch('value').to_d > 0 &&
+            entry['scriptPubKey'].has_key?('addresses')
+        end
+          .each_with_object([]) do |entry, formatted_txs|
+          no_currency_tx =
+            { hash: tx_hash['txid'], txout: entry['n'],
+              to_address: entry['scriptPubKey']['addresses'][0],
+              amount: entry.fetch('value').to_d,
+              status: 'success' }
 
           # Build transaction for each currency belonging to blockchain.
           settings_fetch(:currencies).pluck(:id).each do |currency_id|
